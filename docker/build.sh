b3307ea3a7883d63e5bffed4910318db2d48e5b0
Peter Prescott <prescott.peter@gmail.com>
2020-11-24 22:44:55 +0000

    Update Docker configuration with  folder mounted as volume and BUILDER variable adjustable in  file

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,7 @@
+# -- Get Builder's Username
+
+source ../.env
+
 # -- Software Stack Version
 
 SPARK_VERSION="3.0.0"
@@ -7,25 +11,32 @@ JUPYTERLAB_VERSION="2.1.5"
 # -- Building the Images
 
 docker build \
-  -f cluster-base.Dockerfile \
-  -t cluster-base .
+  -f ./cluster_base.Dockerfile \
+  -t ${BUILDER}/cluster-base .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
-  -f spark-base.Dockerfile \
-  -t spark-base .
+  -f ./spark-base.Dockerfile \
+  -t ${BUILDER}/spark-base .
 
 docker build \
-  -f spark-master.Dockerfile \
-  -t spark-master .
+  -f ./spark-master.Dockerfile \
+  -t ${BUILDER}/spark-master .
 
 docker build \
-  -f spark-worker.Dockerfile \
-  -t spark-worker .
+  -f ./spark-worker.Dockerfile \
+  -t ${BUILDER}/spark-worker .
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
-  -f jupyterlab.Dockerfile \
-  -t jupyterlab .
+  -f ./jupyterlab.Dockerfile \
+  -t ${BUILDER}/jupyterlab .
+
+
+docker push ${BUILDER}/cluster-base
+docker push ${BUILDER}/spark-base
+docker push ${BUILDER}/spark-master
+docker push ${BUILDER}/spark-worker
+docker push ${BUILDER}/jupyterlab
